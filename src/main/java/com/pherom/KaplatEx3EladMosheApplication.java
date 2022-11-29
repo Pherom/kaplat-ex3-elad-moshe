dc88c5bb72f613733da4c872778981a12a278d6b
Elad Moshe <pheromem@gmail.com>
2022-11-29 15:25:42 +0200

    Initialized Spring project

---
+++ src/main/java/com/pherom/KaplatEx3EladMosheApplication.java
@@ -0,0 +1,13 @@
+package com.pherom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaplatEx3EladMosheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaplatEx3EladMosheApplication.class, args);
+	}
+
+}
